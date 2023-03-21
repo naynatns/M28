d3b833e241efe909c0ea472a54c85bb96bb26038
Hp <Hp@DESKTOP-MEHK842>
2023-03-21 22:52:27 +0530

    day three

---
+++ DayThree/src/com/tnsif/daythree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.daythree.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+
+		OopsConceptDemo obj=new OopsConceptDemo();
+		obj.setName("Nayna");
+		obj.setAge(22);
+		obj.setSerialNum(1);
+		System.out.println(obj);
+		
+		
+
+		
+		
+	}
+
+}
---
+++ DayThree/src/com/tnsif/daythree/encapsulation/OopsConceptDemo.java
@@ -0,0 +1,42 @@
+package com.tnsif.daythree.encapsulation;
+
+public class OopsConceptDemo {
+	
+	//member variables
+	private int serialNum;
+	private String name;
+	private int age;
+	
+	//methods
+	public int getSerialNum() {
+		return serialNum;
+	}
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	//Object Class -method-to return string representation of the object
+	
+	@Override
+	public String toString() {
+		return "OopsConceptDemo [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+	
+	
+	
+
+}

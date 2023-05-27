51f683d65349e2deeff219eed7814db18540b842
Hp <Hp@DESKTOP-MEHK842>
2023-05-27 16:46:55 +0530

    JPA Inheritance

---
+++ JPAJSInheritance/src/com/cg/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,36 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();
+		employee.setName("Rahul");
+		employee.setSalary(55000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager = new Manager();
+		manager.setName("Sahim");
+		manager.setSalary(85000);
+		manager.setDepartmentName("Training");
+		em.persist(manager);
+		
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
